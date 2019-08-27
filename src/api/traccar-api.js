e1159a875c9eca35ccd8d8297461ab61aa542340
jcardus <joaquim.cardeira@gmail.com>
2019-08-28 00:40:11 +0100

    trips report

--- src/api/traccar-api.js
+++ src/api/traccar-api.js
@@ -1,25 +1,17 @@
 import Vue from 'vue'
 import VueCookies from 'vue-cookies'
 import axios from 'axios'
-import VueNativeSock from 'vue-native-websocket'
 import { vm } from '../main.js'
-
 const serverHost = 'dev.pinme.io'
 const baseUrl = 'https://' + serverHost + '/api/'
 const devices = baseUrl + 'devices'
 const positions = baseUrl + 'reports/route'
 const trips = baseUrl + 'reports/trips'
-
 const cookie = VueCookies.get('user-info')
+
 export const traccar = {
   startReceiving: function() {
-    Vue.use(VueNativeSock, 'wss://' + serverHost + '/api/socket', {
-      store: vm.$store,
-      format: 'json',
-      reconnection: true,
-      reconnectionAttempts: 5,
-      reconnectionDelay: 3000
-    })
+    vm.$connect()
   },
   devices: function(onFulfill) {
     axios.get(devices, { withCredentials: true, auth: { username: cookie.email, password: cookie.password }})
@@ -51,6 +43,9 @@ export const traccar = {
       .catch(reason => {
         Vue.$log.error(reason)
       })
+  },
+  stopReceiving: function() {
+    vm.$disconnect()
   }
 }
 
