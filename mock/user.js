a7f318ab917ca44e8f0e4c792987acafcf411482
jcardus <joaquim.cardeira@gmail.com>
2019-08-27 02:11:51 +0100

    trips report

--- mock/user.js
+++ mock/user.js
@@ -12,13 +12,13 @@ const users = {
   'admin-token': {
     roles: ['admin'],
     introduction: 'I am a super administrator',
-    avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+    avatar: 'https://ui-avatars.com/api/?name=John+Doe',
     name: 'Super Admin'
   },
   'editor-token': {
     roles: ['editor'],
     introduction: 'I am an editor',
-    avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+    avatar: 'https://ui-avatars.com/api/?name=John+Doe',
     name: 'Normal Editor'
   }
 }
