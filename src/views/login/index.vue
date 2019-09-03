cc0cf6d94170537f67c1167d4975ba642487cfed
jcardus <joaquim.cardeira@gmail.com>
2019-09-04 00:01:54 +0100

    login without importing i18n

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -62,7 +62,7 @@ export default {
     }
     const validatePassword = (rule, value, callback) => {
       if (value.length < 6) {
-        callback(new Error(this.t('login_password_warn')))
+        callback(new Error(this.$t('login_password_warn')))
       } else {
         callback()
       }
@@ -168,8 +168,6 @@ export default {
 </script>
 
 <style lang="scss">
-/* 修复input 背景不协调 和光标变色 */
-/* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 
 $bg:#283443;
 $light_gray:#fff;
