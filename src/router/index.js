993116e50c50dfac47f9b7d5c2e791704c9f2b32
jcardus <joaquim.cardeira@gmail.com>
2019-11-13 03:36:11 +0000

    removed garbage

--- src/router/index.js
+++ src/router/index.js
@@ -55,16 +55,6 @@ export const constantRoutes = [
     hidden: true
   },
   {
-    path: '/404',
-    component: () => import('@/views/error-page/404'),
-    hidden: true
-  },
-  {
-    path: '/401',
-    component: () => import('@/views/error-page/401'),
-    hidden: true
-  },
-  {
     path: '/',
     redirect: '/map',
     hidden: false,
