920991c57c9ee9b6b6c5a1046eb436612b319f9c
jcardus <joaquim.cardeira@gmail.com>
2019-11-23 11:00:45 -0300

    element ui css was being imported twice...

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import 'normalize.css/normalize.css' // a modern alternative to CSS resets
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 import './styles/element-variables.scss'
 import '@/styles/index.scss' // global css
 import App from './App'
