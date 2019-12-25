a7d529bda6be907365e388b72805fab298947b17
jcardus <joaquim.cardeira@gmail.com>
2019-12-25 19:52:59 +0000

    chagend reports url

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -3,4 +3,4 @@ export const rotateStep = 9
 export const routeSlotLength = 6
 export const routePlaySpeed = 0.0005
 export const refreshRate = 55
-export const rServerUrl = 'https://8w09p74n5g.execute-api.us-east-1.amazonaws.com/Prod/report'
+export const rServerUrl = 'https://s3emhl8duc.execute-api.us-east-1.amazonaws.com/Prod/report'
