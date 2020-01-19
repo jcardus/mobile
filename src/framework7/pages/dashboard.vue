0460bb41e1806ff49ba4b39d2b3a8601d5e2166d
jcardus <joaquim.cardeira@gmail.com>
2020-01-19 22:11:16 +0000

    dashboard page on mobile

---
+++ src/framework7/pages/dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+  <f7-page>
+    <dashboard></dashboard>
+  </f7-page>
+</template>
+
+<script>
+import Dashboard from '../../views/dashboard/Dashboard'
+
+export default {
+  name: 'Dashboard',
+  components: { Dashboard }
+}
+</script>
+
+<style scoped>
+
+</style>
