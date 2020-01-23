85910aa0acd8a85885dd01b5afa2bdee24d9155c
jcardus <joaquim.cardeira@gmail.com>
2020-01-23 21:40:53 +0000

    313

--- src/service-worker.js
+++ src/service-worker.js
@@ -39,13 +39,6 @@ if (workbox) {
 
   // eslint-disable-next-line no-undef
   workbox.routing.registerRoute(
-    new RegExp('https://.*\\.amazonaws.com/v1/.*'),
-    // eslint-disable-next-line no-undef
-    new workbox.strategies.NetworkOnly()
-  )
-
-  // eslint-disable-next-line no-undef
-  workbox.routing.registerRoute(
     /^https:\/\/fonts\.googleapis\.com/,
     // eslint-disable-next-line no-undef
     new workbox.strategies.CacheFirst({
