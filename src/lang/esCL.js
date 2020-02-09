936867e149edb277464ffd4fecfd6b0eaa437c9e
jcardus <joaquim.cardeira@gmail.com>
2020-02-09 23:57:12 +0000

    spanish translation

--- src/lang/esCL.js
+++ src/lang/esCL.js
@@ -35,9 +35,9 @@ export default {
     column_speed: 'Km/h',
     column_lastUpdate: 'Ultima actualización',
     order_by_status: 'Ordenar por Estado',
-    order_by_vehicle: 'Ordenar por Veículo',
+    order_by_vehicle: 'Ordenar por Vehículo',
     order_by_group: 'Ordenar por Grupo',
-    order_by_last_update: 'Ordenar pela última actualização'
+    order_by_last_update: 'Ordenar pela ultima actualización'
   },
   vehicleTable: {
     immobilize: 'Inmovilizar',
