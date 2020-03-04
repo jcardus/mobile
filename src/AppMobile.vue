c03c03b152dc753779251120e6b504f054ddde2b
jcardus <joaquim.cardeira@gmail.com>
2020-03-04 11:22:25 +0000

    starging dashboard cosmetics

--- src/AppMobile.vue
+++ src/AppMobile.vue
@@ -12,7 +12,7 @@
       </f7-toolbar>
       <f7-view id="view-map" main tab tab-active url="/map" @tab:show="mapShow"></f7-view>
       <f7-view id="view-reports" name="reports" tab url="/reports" @tab:show="reportsShow"></f7-view>
-      <f7-view id="view-dashboard" name="dashboard" tab url="/dashboard"></f7-view>
+      <f7-view id="view-dashboard" name="dashboard" tab url="/dashboard" @tab:show="dashboardShow"></f7-view>
       <f7-view id="view-settings" name="settings" tab url="/settings"></f7-view>
       <f7-view id="view-login" name="login" url="/login"></f7-view>
     </f7-views>
@@ -164,6 +164,10 @@ export default {
         closeTimeout: 5000,
         subtitle: partner.getTitle()
       }).open()
+    },
+    dashboardShow() {
+      Vue.$log.debug('emit dashboardActive')
+      serverBus.$emit('dashboardActive')
     },
     reportsShow() {
       Vue.$log.debug('emit reportsActive')
