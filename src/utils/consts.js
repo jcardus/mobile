88bed719f26b6668f9b12ee8666747ff49249afc
jcardus <joaquim.cardeira@gmail.com>
2020-03-21 00:26:11 +0000

    moto icon

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -5,7 +5,7 @@ export const routePlaySpeed = 0.0005
 export const refreshRate = 55
 export const backEndHostName = 'xmjth8acs5.execute-api.us-east-1.amazonaws.com'
 export const rServerUrl = 'https://' + backEndHostName + '/Prod/report'
-export const spriteUrl = 'https://3dicons.s3.amazonaws.com/sprite/3/sprite'
-// export const spriteUrl = 'https://localhost:8080/img/sprite'
+export const spriteUrl = 'https://3dicons.s3.amazonaws.com/sprite/5/sprite'
+// export const spriteUrl = 'https://mac.pinme.io:8080/img/sprite'
 export const mapboxSprite = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/sprite' // @2x.png?access_token=
 export const mapboxAccessToken = 'pk.eyJ1IjoiamNhcmRlaXJhMiIsImEiOiJjang4OXJmN2UwaGNxM3BwbjY2ZGFjdGw1In0.6NPI_KuClrH_OrP4NN3oeQ'
