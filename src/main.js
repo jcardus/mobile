19e50ae0c587bcea4cd38b93799f2c54889648a7
jcardus <joaquim.cardeira@gmail.com>
2020-03-21 12:30:36 +0000

    service worker

--- src/main.js
+++ src/main.js
@@ -82,21 +82,24 @@ if ('serviceWorker' in navigator) {
   Vue.$log.debug('registering service worker...')
   navigator.serviceWorker.register('/sw.js').then(reg => {
     regServiceWorker = reg
-    reg.addEventListener('updatefound', () => {
-      Vue.$log.debug('A wild service worker has appeared in reg.installing!')
-      newServiceWorker = reg.installing
-      newServiceWorker.addEventListener('statechange', () => {
-        // Has network.state changed?
-        if (newServiceWorker.state === 'installed') {
-          if (navigator.serviceWorker.controller) {
-            // new update available
-            serverBus.$emit('updateAvailable')
-          }
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.getRegistrations().then((registrations) => {
+        for (const reg of registrations) {
+          reg.addEventListener('updatefound', () => {
+            Vue.$log.debug('A wild service worker has appeared in reg.installing!')
+            newServiceWorker = reg.installing
+            newServiceWorker.addEventListener('statechange', () => {
+              // Has network.state changed?
+              if (newServiceWorker.state === 'installed') {
+                if (navigator.serviceWorker.controller) {
+                  serverBus.$emit('updateAvailable')
+                }
+              }
+            })
+          })
         }
       })
-    })
-  }).catch((e) => {
-    Vue.$log.error(e)
+    }
   })
   let refreshing
   navigator.serviceWorker.addEventListener('controllerchange', function() {
