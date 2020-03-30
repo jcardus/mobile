54572c8d2c10f38bad5a98441fe1dd68894177f7
jcardus <joaquim.cardeira@gmail.com>
2020-03-30 01:14:44 +0100

    starting alertdetail before moving devices to store

---
+++ src/framework7/pages/alerts/AlertDetail.vue
@@ -0,0 +1,46 @@
+<template>
+  <f7-page name="AlertDetail">
+    <f7-navbar back-link :title="$t('route.alerts')"></f7-navbar>
+    <f7-block-title>{{ events[alertId].title }}</f7-block-title>
+    <f7-block>
+      <f7-row>
+        <f7-col width="40">
+          {{ events[alertId].type }}
+        </f7-col>
+        <f7-col width="60">
+          {{ events[alertId].timestamp | moment('LLLL') }}
+        </f7-col>
+      </f7-row>
+      <div style="height: 500px">
+        <simple-map
+          center=""
+        />
+      </div>
+    </f7-block>
+  </f7-page>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import SimpleMap from '../../../views/map/SimpleMap'
+
+export default {
+  name: 'AlertDetail',
+  components: { SimpleMap },
+  data() {
+    return {
+      alertId: 0
+    }
+  },
+  computed: {
+    ...mapGetters(['events'])
+  },
+  mounted() {
+    this.alertId = parseInt(this.$f7route.params.alertId)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/map/SimpleMap.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="simpleMap" style="width: 100%; height: 100%">
+  </div>
+</template>
+
+<script>
+import mapboxgl from 'mapbox-gl'
+export default {
+  name: 'SimpleMap',
+  static() {
+    return {
+      simpleMap: null
+    }
+  },
+  mounted() {
+    this.$static.simpleMap = new mapboxgl.Map({
+      container: 'simpleMap',
+      center: [-74.5, 40], // starting position [lng, lat]
+      style: 'mapbox://styles/mapbox/streets-v11'
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
