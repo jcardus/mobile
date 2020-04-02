1461986063fffba282c751631363b1326aa71f03
jppenas <jppenas@gmail.com>
2020-04-02 01:29:41 +0100

    Fix account edit

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -3,7 +3,7 @@
     <div v-if="user">
       <el-row :gutter="20">
         <el-col :span="12" :xs="24">
-          <account :user="user" />
+          <account></account>
         </el-col>
       </el-row>
     </div>
@@ -17,40 +17,20 @@ import { mapGetters } from 'vuex'
 export default {
   name: 'Profile',
   components: { Account },
-  data() {
-    return {
-      user: {}
-    }
-  },
   computed: {
     ...mapGetters(['user']),
     top() {
       if (('standalone' in window.navigator) && window.navigator.standalone) {
-        return 'padding-top:100px;'
+        return 'padding-top:70px;'
       }
-      return 'padding-top:60px'
-    }
-  },
-  created() {
-    this.getUser()
-  },
-  methods: {
-    getUser() {
-      this.user = {
-        name: this.user.name,
-        email: this.user.email,
-        password: this.user.password,
-        phone: this.user.phone,
-        timezone: this.user.attributes.timezone,
-        language: this.user.attributes.lang
-      }
+      return 'padding-top:30px'
     }
   }
 }
 </script>
 <style scoped>
   .app-container {
-    padding-top: 60px;
+    padding-top: 10px;
     padding-right: 10px;
     padding-left: 10px;
   }
