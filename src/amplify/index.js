b82a5f8e7bc43a797f2987fcc8be16ce54da8a4a
jcardus <joaquim.cardeira@gmail.com>
2020-04-11 04:30:07 +0100

    delete cookie

--- src/amplify/index.js
+++ src/amplify/index.js
@@ -1,4 +1,6 @@
 import Amplify from 'aws-amplify'
 import awsConfig from '../aws-exports'
 
+awsConfig.oauth.redirectSignIn = 'https://' + window.location.host + '/googlelogin/'
+console.log(awsConfig)
 Amplify.configure(awsConfig)
