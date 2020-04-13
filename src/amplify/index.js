c3e36323f980c4a415e6fa42f0f9b15cc59b73b5
jcardus <joaquim.cardeira@gmail.com>
2020-04-13 02:50:57 +0100

    adding pinme api fixed vehicle list height fixed google login

--- src/amplify/index.js
+++ src/amplify/index.js
@@ -4,7 +4,7 @@ import { getServerHost } from '../api'
 
 const url = 'https://' + getServerHost() + '/pinmeapi'
 
-awsConfig.oauth.redirectSignIn = window.location.protocol + '//' + window.location.host + '/googlelogin'
+awsConfig.oauth.redirectSignIn = window.location.protocol + '//' + window.location.host + '/googlelogin/'
 awsConfig.aws_cloud_logic_custom[0].endpoint = url
 awsConfig.aws_cloud_logic_custom[0].custom_header = async() => {
   return { Authorization: `Bearer ${(await Auth.currentSession()).getAccessToken().getJwtToken()}` }
