88ad484e4a2ac3e26e10a1ccf5e9a35f93aa97fc
jcardus <joaquim.cardeira@gmail.com>
2020-04-17 13:53:49 +0100

    labels color

--- src/utils/partner.js
+++ src/utils/partner.js
@@ -18,6 +18,7 @@ export function getThemeColor() {
 export function getCSSName() {
   if (hostname === 'wuizy.co.ao') { return 'wuizy' }
   if (hostname === 'web.fleetrack.cl') { return 'fleetrack' }
+  if (hostname === 'www.fleetrackchile.com') { return 'fleetrack' }
   if (hostname === 'map.able-on.mobi') { return 'able-on' }
   return 'wuizy'
 }
@@ -25,6 +26,7 @@ export function getCSSName() {
 export function getTitle() {
   if (hostname === 'wuizy.co.ao') { return 'WuizyGo' }
   if (hostname === 'web.fleetrack.cl') { return 'Fleetrack' }
+  if (hostname === 'www.fleetrackchile.com') { return 'Fleetrack' }
   if (hostname === 'map.able-on.mobi') { return 'able-on' }
   return 'Pinme'
 }
