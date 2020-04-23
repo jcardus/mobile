6f902b515c768f453457234ad7b0a42ff26fa99b
jcardus <joaquim.cardeira@gmail.com>
2020-04-23 19:36:36 +0100

    dev backend url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,16 @@
 import awsConfig from '../aws-exports'
 
+const backendProd = 'xmjth8acs5.'
+const backendDev = 'wd5b3zc65f'
+const backendUrl = 'execute-api.us-east-1.amazonaws.com'
+
 export function getGoogleRedirect() {
   return window.location.protocol + '//' + window.location.host + '/googlelogin/'
+}
+
+export function getBackendHost() {
+  const appName = window.location.hostname.includes('dev.') ? backendDev : backendProd
+  return appName + '.' + backendUrl
 }
 
 export function getServerHost() {
