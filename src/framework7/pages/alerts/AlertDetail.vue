d91f047496bafe0331c0e9a7cde6a8e3924103c5
jppenas <jppenas@gmail.com>
2020-07-15 01:55:56 +0100

    Add new alert types

--- src/framework7/pages/alerts/AlertDetail.vue
+++ src/framework7/pages/alerts/AlertDetail.vue
@@ -1,7 +1,7 @@
 <template>
   <f7-page name="AlertDetail">
     <f7-navbar back-link="" :title="$t('route.alerts')"></f7-navbar>
-    <f7-block-title v-if="alert" medium>{{ alert.type }}</f7-block-title>
+    <f7-block-title v-if="alert" medium>{{ alert.description }}</f7-block-title>
     <f7-block>
       <f7-list v-if="alert">
         <f7-list-item :title="alert.title" :after="alert.timestamp | moment('calendar')" />
