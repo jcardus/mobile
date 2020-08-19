b46c610bc0ad4f113fe35d55cb818e8a716e69a5
jcardus <joaquim.cardeira@inosat.pt>
2020-08-19 16:16:19 +0100

    freshchat in csp

--- src/utils/partner.js
+++ src/utils/partner.js
@@ -1,7 +1,23 @@
 import styles from '../styles/element-variables.scss'
 
+function initFreshChat() {
+  window.fcWidget.init({
+    token: '022233f4-44bb-44b6-bdb3-7ff6a0a61515',
+    host: 'https://wchat.eu.freshchat.com'
+  })
+}
+function initialize(i, t) {
+  let e // noinspection CommaExpressionJS
+  i.getElementById(t) ? initFreshChat() : ((e = i.createElement('script')).id = t, e.async = !0, e.src = 'https://wchat.eu.freshchat.com/js/widget.js', e.onload = initFreshChat, i.head.appendChild(e))
+} function initiateCall() {
+  initialize(document, 'freshchat-js-sdk')
+}
+
 export function initSupportChat() {
-  if (hostname === 'mac.pinme.io' || hostname === 'web.fleetrack.cl') {
+  if (hostname === 'mac.pinme.io') {
+    window.addEventListener ? window.addEventListener('load', initiateCall, !1) : window.attachEvent('load', initiateCall, !1)
+  }
+  if (hostname === 'web.fleetrack.cl') {
     const el = document.createElement('script')
     el.setAttribute('src', 'https://static.zdassets.com/ekr/snippet.js?key=1a5e99d9-66d0-4187-abf9-0870a6768cda')
     el.setAttribute('id', 'ze-snippet')
