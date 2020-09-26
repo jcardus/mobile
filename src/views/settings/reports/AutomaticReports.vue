680b983d022507ccba80de68ead24777c3090be1
jpenas <jppenas@gmail.com>
2020-09-26 01:46:43 +0100

    Interface to configure automatic reports

---
+++ src/views/settings/reports/AutomaticReports.vue
@@ -0,0 +1,188 @@
+<template>
+  <div>
+    <h3>{{ $t('settings.automatic_reports') }}</h3>
+    <el-form ref="user" :model="user" label-width="auto">
+      <div>
+        <el-tooltip :content="$t('profile.inactive_vehicles_email_tooltip')">
+          <el-switch
+            v-model="inactiveVehiclesEmail"
+            :active-text="$t('profile.inactive_vehicles_email')"
+            style="padding-right:10px"
+            @change="dirty=true"
+          >
+          </el-switch>
+        </el-tooltip>
+      </div>
+      <div class="reportOptions">
+        <span v-if="inactiveVehiclesEmail">{{ $t('profile.inactive_vehicles_email_tooltip') }}</span>
+      </div>
+      <div>
+        <el-tooltip class="item" :content="$t('settings.activate_automatic_trip_report')">
+          <el-switch
+            v-model="trip_report.isactive"
+            :active-text="$t('route.report_trip_title')"
+            style="padding-right:10px"
+            @change="dirty=true"
+          ></el-switch>
+        </el-tooltip>
+      </div>
+      <div class="reportOptions">
+        <el-radio-group v-if="trip_report.isactive" v-model="trip_report.periodicity">
+          <el-radio label="daily">{{ $t('settings.report_periodicity_daily') }}</el-radio>
+          <el-radio label="weekly">{{ $t('settings.report_periodicity_weekly') }}</el-radio>
+          <el-radio label="monthly">{{ $t('settings.report_periodicity_monthly') }}</el-radio>
+        </el-radio-group>
+      </div>
+      <div>
+        <el-tooltip class="item" :content="$t('settings.activate_automatic_location_report')">
+          <el-switch
+            v-model="location_report.isactive"
+            :active-text="$t('route.report_location_title')"
+            style="padding-right:10px"
+            @change="dirty=true"
+          ></el-switch>
+        </el-tooltip>
+      </div>
+      <div class="reportOptions">
+        <el-radio-group v-if="location_report.isactive" v-model="location_report.periodicity">
+          <el-radio label="daily">{{ $t('settings.report_periodicity_daily') }}</el-radio>
+          <el-radio label="weekly">{{ $t('settings.report_periodicity_weekly') }}</el-radio>
+          <el-radio label="monthly">{{ $t('settings.report_periodicity_monthly') }}</el-radio>
+        </el-radio-group>
+      </div>
+      <div>
+        <el-tooltip class="item" :content="$t('settings.activate_automatic_zone_report')">
+          <el-switch
+            v-model="zone_report.isactive"
+            :active-text="$t('route.report_zone_crossing')"
+            style="padding-right:10px"
+            @change="dirty=true"
+          ></el-switch>
+        </el-tooltip>
+      </div>
+      <div class="reportOptions">
+        <el-radio-group v-if="zone_report.isactive" v-model="zone_report.periodicity">
+          <el-radio label="daily">{{ $t('settings.report_periodicity_daily') }}</el-radio>
+          <el-radio label="weekly">{{ $t('settings.report_periodicity_weekly') }}</el-radio>
+          <el-radio label="monthly">{{ $t('settings.report_periodicity_monthly') }}</el-radio>
+        </el-radio-group>
+      </div>
+      <div>
+        <el-tooltip class="item" :content="$t('settings.activate_automatic_speeding_report')">
+          <el-switch
+            v-model="speeding_report.isactive"
+            :active-text="$t('route.report_speeding')"
+            style="padding-right:10px"
+            @change="dirty=true"
+          ></el-switch>
+        </el-tooltip>
+      </div>
+      <div class="reportOptions">
+        <el-radio-group v-if="speeding_report.isactive" v-model="speeding_report.periodicity">
+          <el-radio value="daily" label="daily">{{ $t('settings.report_periodicity_daily') }}</el-radio>
+          <el-radio value="weekly" label="weekly">{{ $t('settings.report_periodicity_weekly') }}</el-radio>
+          <el-radio value="monthly" label="monthly">{{ $t('settings.report_periodicity_monthly') }}</el-radio>
+        </el-radio-group>
+      </div>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button :loading="loading" type="primary" :disabled="!dirty" @click="submit">{{ $t('profile.user_update_button') }}</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { vm } from '../../../main'
+import * as lnglat from '../../../utils/lnglat'
+import { mapGetters } from 'vuex'
+import { traccar } from '@/api/traccar-api'
+import { setLanguage } from '@/lang'
+
+export default {
+  name: 'AutomaticReports',
+  data() {
+    return {
+      dirty: false,
+      loading: false,
+      inactiveVehiclesEmail: false,
+      trip_report: {
+        isactive: false,
+        periodicity: 'daily'
+      },
+      location_report: {
+        isactive: false,
+        periodicity: 'daily'
+      },
+      zone_report: {
+        isactive: false,
+        periodicity: 'daily'
+      },
+      speeding_report: {
+        isactive: false,
+        periodicity: 'daily'
+      }
+    }
+  },
+  computed: {
+    ...mapGetters(['avatar', 'user']),
+    isMobile() { return lnglat.isMobile() },
+    devices: function() {
+      return vm.$store.getters.devices
+    }
+  },
+  mounted() {
+    Vue.$log.debug(this.user)
+    if (this.user.attributes.inactiveVehiclesEmail !== undefined) {
+      this.inactiveVehiclesEmail = this.user.attributes.inactiveVehiclesEmail
+    }
+    if (this.user.attributes.trip_report !== undefined) {
+      this.trip_report = this.user.attributes.trip_report
+    }
+    if (this.user.attributes.location_report !== undefined) {
+      this.location_report = this.user.attributes.location_report
+    }
+    if (this.user.attributes.zone_report !== undefined) {
+      this.zone_report = this.user.attributes.zone_report
+    }
+    if (this.user.attributes.speeding_report !== undefined) {
+      this.speeding_report = this.user.attributes.speeding_report
+    }
+  },
+  methods: {
+    submit() {
+      this.loading = true
+      this.user.attributes.inactiveVehiclesEmail = this.inactiveVehiclesEmail
+      this.user.attributes.trip_report = this.trip_report
+      this.user.attributes.location_report = this.location_report
+      this.user.attributes.zone_report = this.zone_report
+      this.user.attributes.speeding_report = this.speeding_report
+      traccar.updateUser(this.user.id, this.user
+      ).then(({ data }) => {
+        this.dirty = false
+        setLanguage(data.attributes.lang)
+        this.$message({
+          message: this.$t('profile.user_updated'),
+          type: 'success',
+          duration: 5 * 1000
+        })
+      }).catch((e) => {
+        this.$message({
+          message: e,
+          type: 'error',
+          duration: 5 * 1000
+        })
+      }).finally(() => { this.loading = false })
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  @import '../../../styles/element-variables.scss';
+
+  .reportOptions{
+    margin-top: 10px;
+    margin-bottom: 20px;
+  }
+</style>
