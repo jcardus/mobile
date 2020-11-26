a405018fbed01e8d850d4056f01852d6f7a55dfa
jpenas <jppenas@gmail.com>
2020-11-26 10:50:59 +0000

    Show group name in the vehicle list

--- src/views/map/VehicleTable.vue
+++ src/views/map/VehicleTable.vue
@@ -98,6 +98,7 @@
           <template slot-scope="scope">
             <div style="padding: 3px 0 0;">
               <span style="font-weight: bold">{{ scope.row.name }} </span>
+              <span style="float: right; font-size: smaller">{{ scope.row.groupId | formatGroup }} </span>
             </div>
             <div v-if="scope.row.position && scope.row.position.attributes.driverUniqueId && scope.row.position.attributes.driverUniqueId !== '0'" style="padding-top: 2px; line-height: normal">
               <span style="font-size: 12px;"><i class="fas fa-user driverIcon"></i>{{ scope.row.driver.name }}</span>
