71cc74ab32a94d7dc9a7d40e421c4b206e695841
jpenas <jppenas@gmail.com>
2021-02-01 01:53:56 +0000

    HistoryMode - show fuel consumption

--- src/views/map/TripTable.vue
+++ src/views/map/TripTable.vue
@@ -18,7 +18,7 @@
         >
           <template slot-scope="scope">
             <div
-              style="display: flow-root; margin-top: 5px; border-radius:10px; padding-left: 10px; padding-bottom: 5px;padding-right: 10px"
+              style="display: flow-root; margin-top: 5px; border-radius:10px; padding-left: 5px; padding-bottom: 5px;padding-right: 5px"
               :class="currentTrip===scope.row ? 'tripSelectedBackground' : ''"
             >
               <div :class="getTripIndexClass(scope.row)" style="background-color: #FFFFFF ; border-radius:10px">
@@ -73,14 +73,19 @@
                 </el-col>
               </el-row>
               <el-row>
-                <el-col :span="12" class="colTripData">
+                <el-col :span="scope.row.xpert ? 8 : 12" class="colTripData">
                   <div>
-                    <span style="font-size: 12px"><i class="fas fa-road" style="width: 15px; color: black"></i> {{ scope.row.trip_distance }} km</span>
+                    <span style="font-size: 12px"><i class="fas fa-road" style="width: 15px; color: black"></i> {{ scope.row.trip_distance }}km</span>
                   </div>
                 </el-col>
-                <el-col :span="12" class="colTripData">
+                <el-col :span="scope.row.xpert ? 8 : 12" class="colTripData">
                   <div>
-                    <span style="font-size: 12px"><i class="fas fa-tachometer-alt" style="color: #13ce66"></i> {{ scope.row.trip_avg_speed }} km/h </span>
+                    <span style="font-size: 12px"><i class="fas fa-tachometer-alt" style="color: #13ce66"></i> {{ scope.row.trip_avg_speed }}km/h </span>
+                  </div>
+                </el-col>
+                <el-col v-if="scope.row.xpert" :span="8" class="colTripData">
+                  <div>
+                    <span style="font-size: 12px"><i class="fas fa-gas-pump" style="color: #13ce66"></i> {{ scope.row.fuel_consumption }}L</span><span style="font-size: 10px"> ({{ scope.row.avg_fuel_consumption }}L\100)</span>
                   </div>
                 </el-col>
               </el-row>
@@ -102,7 +107,8 @@
         <div style="width: 33%; float:left"><span style="font-size: 12px"><i class="fas fa-car" style="width: 15px; color: #F5365C"></i> {{ calculateTime(totalStopTime) }}</span></div>
       </div>
       <div>
-        <span style="font-size: 12px"><i class="fas fa-road" style="width: 15px; color: black"></i> {{ totalKms }} km</span>
+        <div style="width: 50%; float:left"><span style="font-size: 12px"><i class="fas fa-road" style="width: 15px; color: black"></i> {{ totalKms }} km</span></div>
+        <div v-if="trips[0].xpert" style="width: 50%; float:left"><span style="font-size: 12px"><i class="fas fa-gas-pump" style="color: #13ce66"></i> {{ totalFuelConsumption }}L</span><span style="font-size: 10px"> ({{ avgFuelConsumption }}L\100)</span></div>
       </div>
     </div>
   </div>
@@ -136,6 +142,13 @@ export default {
     },
     totalKms() {
       return Math.round(this.trips.reduce((sum, t) => sum + t.trip_distance, 0))
+    },
+    totalFuelConsumption() {
+      return Math.round(this.trips.reduce((sum, t) => sum + t.fuel_consumption, 0))
+    },
+    avgFuelConsumption() {
+      const totalKms = this.totalKms
+      return Math.round(totalKms > 0 ? this.totalFuelConsumption * 100 / totalKms : 0)
     },
     totalDrivingTime() {
       return this.trips.reduce((sum, t) => sum + t.trip_driving_time, 0)
