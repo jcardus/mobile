f3b488473c3cde5649729b6f1931c00160175b22
jcardus <asklocation.net@gmail.com>
2021-06-11 18:23:10 -0400

    preview

--- build/index.js
+++ build/index.js
@@ -1,35 +1,20 @@
-const { run } = require('runjs')
 const chalk = require('chalk')
 const config = require('../vue.config.js')
-const rawArgv = process.argv.slice(2)
-const args = rawArgv.join(' ')
 
-if (process.env.npm_config_preview || rawArgv.includes('--preview')) {
-  const report = rawArgv.includes('--report')
+const port = 9526
+const publicPath = config.publicPath
 
-  run(`vue-cli-service build ${args}`)
+const connect = require('connect')
+const serveStatic = require('serve-static')
+const app = connect()
 
-  const port = 9526
-  const publicPath = config.publicPath
-
-  var connect = require('connect')
-  var serveStatic = require('serve-static')
-  const app = connect()
-
-  app.use(
-    publicPath,
-    serveStatic('./dist', {
-      index: ['index.html', '/']
-    })
-  )
-
-  app.listen(port, function () {
-    console.log(chalk.green(`> Preview at  http://localhost:${port}${publicPath}`))
-    if (report) {
-      console.log(chalk.green(`> Report at  http://localhost:${port}${publicPath}report.html`))
-    }
-
+app.use(
+  publicPath,
+  serveStatic('./dist', {
+    index: ['index.html', '/']
   })
-} else {
-  run(`vue-cli-service build ${args}`)
-}
+)
+
+app.listen(port, function () {
+  console.log(chalk.green(`> Preview at  http://localhost:${port}${publicPath}`))
+})
