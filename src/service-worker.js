898bef7e71dc263c6debc92ad0850d898c777b96
jcardus <asklocation.net@gmail.com>
2021-06-12 10:53:48 -0400

    cache fontawesome

--- src/service-worker.js
+++ src/service-worker.js
@@ -7,7 +7,7 @@ if (workbox) {
   workbox.precaching.precacheAndRoute(self.__precacheManifest)
   // eslint-disable-next-line no-undef
   workbox.routing.registerRoute(
-    new RegExp('https:\/\/kit-free\.fontawesome\.com/.*'),
+    new RegExp('https:\/\/kit\.fontawesome\.com/.*'),
     // eslint-disable-next-line no-undef
     new workbox.strategies.CacheFirst({
       cacheName: 'fontawesome'
