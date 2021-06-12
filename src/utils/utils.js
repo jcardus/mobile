3e6425bf598ef8c323b357eed5fbad7938f52766
jcardus <asklocation.net@gmail.com>
2021-06-12 00:24:38 -0400

    969 remove window.location.reload

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -105,7 +105,6 @@ export function reload() {
   if (newServiceWorker) {
     Vue.$log.info(newServiceWorker, 'skipWaiting!')
     newServiceWorker.postMessage({ action: 'skipWaiting' })
-    window.location.reload()
   } else {
     Vue.$log.error(`this shouldn't happen`)
   }
