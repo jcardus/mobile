7bc27ce95f5fc6444e059144b252433f75ea07f7
jcardus <asklocation.net@gmail.com>
2021-06-13 12:54:55 -0400

    remove icons set publicPath /mobile/

--- vue.config.js
+++ vue.config.js
@@ -89,7 +89,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  publicPath: '/mobile/',
   outputDir: 'dist',
   assetsDir: 'staticmobile',
   lintOnSave: process.env.NODE_ENV === 'development',
