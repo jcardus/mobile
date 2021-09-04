7a83c68cfb6be6c58e307d392d6a84a39ab40c84
jcardus <asklocation.net@gmail.com>
2021-09-04 01:50:03 +0100

    no mobile path on capacitor

--- vue.config.js
+++ vue.config.js
@@ -89,7 +89,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: `/${process.env.NODE_ENV !== 'capacitor' ? 'mobile/' : ''}`,
+  publicPath: `/${process.env.ENV !== 'capacitor' ? 'mobile/' : ''}`,
   outputDir: 'dist',
   assetsDir: 'staticmobile',
   lintOnSave: process.env.NODE_ENV === 'development',
