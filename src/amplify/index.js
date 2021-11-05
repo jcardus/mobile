ad6365adb7008dd0bbe899a9325322153691359f
jcardus <asklocation.net@gmail.com>
2021-11-05 22:36:07 +0000

    using capacitor in partner data

--- src/amplify/index.js
+++ src/amplify/index.js
@@ -3,7 +3,12 @@ import { Auth } from '@aws-amplify/auth'
 import { getPartnerData } from 'fleetmap-partners'
 import { Capacitor } from '@capacitor/core'
 
-const partnerData = getPartnerData(window.location.hostname)
+const host = Capacitor.isNativePlatform() ? 'capacitor' : window.location.hostname
+
+console.log('getting partner data for', host)
+
+const partnerData = getPartnerData(host)
+
 export const awsConfig = {
   aws_project_region: 'us-east-1',
   aws_cognito_identity_pool_id: 'us-east-1:b886ef89-6a90-4903-96fc-25af82fc629a',
@@ -11,7 +16,7 @@ export const awsConfig = {
   aws_user_pools_id: partnerData.aws_user_pools_id,
   aws_user_pools_web_client_id: partnerData.cognitoClientId,
   oauth: {
-    redirectSignIn: location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '') + '/mobile/',
+    redirectSignIn: Capacitor.isNativePlatform() ? 'https://fleetmap.io/' : (location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '') + '/mobile/'),
     redirectSignOut: location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '') + '/',
     domain: partnerData.oauth_domain,
     scope: [
@@ -24,10 +29,7 @@ export const awsConfig = {
   },
   federationTarget: 'COGNITO_USER_POOLS'
 }
-if (Capacitor.isNative) {
-  console.log(awsConfig)
-  awsConfig.oauth.redirectSignIn = 'https://fleetmap.io/'
-}
+
 Amplify.configure(awsConfig)
 console.log(Auth.configure(awsConfig))
 
