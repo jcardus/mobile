ec01db1961b084753b68434270d011516094e1cc
jcardus <asklocation.net@gmail.com>
2022-05-01 15:53:16 +0100

    simplify devicefarm

--- mobile/build-android.sh
+++ mobile/build-android.sh
@@ -28,19 +28,6 @@ cp mobile/AndroidManifest.xml android/app/src/main/AndroidManifest.xml
 sed -i 's/PACKAGE_NAME/$PACKAGE_NAME/' android/app/src/main/AndroidManifest.xml
 echo "android manifest:"
 cat android/app/src/main/AndroidManifest.xml
-cd android || exit
 
-gem install bundler
-bundle install
-echo "fastlane add_plugin versioning_android"
-bundle exec fastlane add_plugin versioning_android
-echo "fastlane install_plugins"
-bundle exec fastlane install_plugins
-echo "copy AppFile"
-cp ../mobile/Fastfile fastlane
-echo "json_key_file(\"google-secret.json\")" >> fastlane/Appfile
-echo "package_name(\"$PACKAGE_NAME\")" >> fastlane/Appfile
-echo "Appfile"
-cat fastlane/Appfile
 
 
