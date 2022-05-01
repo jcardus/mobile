3ccf8ac5aec0260a3c048dfd9d76284d56c02074
joaquimcardeira <joaquim_cardeira@hotmail.com>
2022-05-01 12:51:52 +0100

    starting new generic build 3.0.1

---
+++ mobile/build.sh
@@ -0,0 +1,44 @@
+export VERSION_NAME=$npm_package_version
+export VERSION_CODE=${VERSION_NAME//./}00000
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+vue-cli-service build --mode capacitor --dest dist
+rm -rf ./android
+cp dist/img/logos/$HOST_NAME.png dist/img/logos/localhost.png
+mkdir resources
+cp -r mobile/$PACKAGE_NAME/resources/* resources
+
+echo "version code:" $VERSION_CODE
+
+npx cap add android
+npx cap sync android
+npx cap copy
+cordova-res android --skip-config --copy
+mkdir android/fastlane
+cp mobile/localizalia/google-services.json android/app/google-services.json
+echo "copy google-secret"
+cp mobile/google-secret.json android
+cat /android/google-secret.json
+echo "copy key store"
+cp mobile/keystore /etc/keystore
+cp mobile/Gemfile* android
+cp mobile/localizalia/AndroidManifest.xml android/app/src/main/AndroidManifest.xml
+ls android/fastlane
+cd android || exit
+
+gem install bundler
+bundle install
+echo "fastlane add_plugin versioning_android"
+bundle exec fastlane add_plugin versioning_android
+echo "fastlane install_plugins"
+bundle exec fastlane install_plugins
+echo "copy AppFile"
+cp ../mobile/Fastfile fastlane
+echo "json_key_file(\"google-secret.json\")" >> fastlane/Appfile
+echo "package_name(\"$PACKAGE_NAME\")" >> fastlane/Appfile
+echo "Appfile"
+cat fastlane/Appfile
+bundle exec fastlane beta
+
+
