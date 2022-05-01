fa729f97b10f826cbc3f13127d3dc86e1e9e9efd
jcardus <asklocation.net@gmail.com>
2022-05-01 01:38:52 +0100

    google-secret in /etc

--- mobile/localizalia/build.sh
+++ mobile/localizalia/build.sh
@@ -22,12 +22,11 @@ npx cap copy
 cordova-res android --skip-config --copy
 mkdir android/fastlane
 cp mobile/localizalia/google-services.json android/app/google-services.json
-cp mobile/google-secret.json android/google-secret.json
+cp mobile/google-secret.json /etc/google-secret.json
 cp mobile/localizalia/AppFile android/fastlane/AppFile
 cp mobile/localizalia/Fastfile android/fastlane/Fastfile
 echo "copy key store"
 cp mobile/keystore /etc/keystore
-ls /etc/keystore
 cp mobile/Gemfile* android
 cp mobile/fastlane/* android/fastlane
 cp mobile/localizalia/AndroidManifest.xml android/app/src/main/AndroidManifest.xml
