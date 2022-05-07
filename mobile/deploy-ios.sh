c04d47b5b2e0f94ff4158c1d023b04bbcd565fe7
jcardus <asklocation.net@gmail.com>
2022-05-07 18:55:00 -0400

    tesflight

--- mobile/deploy-ios.sh
+++ mobile/deploy-ios.sh
@@ -18,10 +18,11 @@ ruby addGooglePlist.rb
 echo "add_plugin versioning"
 bundle exec fastlane add_plugin versioning
 bundle exec fastlane install_plugins
-echo "pv:$PACKAGE_VERSION"
+
 echo "package_name(\"$PACKAGE_NAME\")" >> fastlane/Appfile
 echo "apple_id(\"admin@fleetmap.io\")" >> fastlane/Appfile
 echo "itc_team_id(\"122303819\")" >> fastlane/Appfile
 echo "team_id(\"57X9MD32BX\")" >> fastlane/Appfile
 echo "Appfile"
+export PACKAGE_VERSION=$PACKAGE_VERSION
 bundle exec fastlane ios beta
