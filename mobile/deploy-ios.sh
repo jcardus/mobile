5027adc8112185b640a9cfd527a19e6fa6ce6367
jcardus <asklocation.net@gmail.com>
2022-05-06 20:35:15 -0400

    fix package_version

--- mobile/deploy-ios.sh
+++ mobile/deploy-ios.sh
@@ -1,4 +1,11 @@
 cp mobile/Gemfile* ios/App
+
+export PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+
 cd ios/App
 gem install bundler
 bundle install
@@ -7,11 +14,6 @@ ruby addGooglePlist.rb
 echo "add_plugin versioning"
 bundle exec fastlane add_plugin versioning
 bundle exec fastlane install_plugins
-export PACKAGE_VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g')
 echo $PACKAGE_VERSION
 echo "package_name(\"$PACKAGE_NAME\")" >> fastlane/Appfile
 echo "apple_id(\"admin@fleetmap.io\")" >> fastlane/Appfile
