66012393d60db154f574ab39ac574bcbd1696745
jcardus <asklocation.net@gmail.com>
2022-05-07 21:28:54 -0400

    fix google-services.plist

--- mobile/build-ios.sh
+++ mobile/build-ios.sh
@@ -9,7 +9,7 @@ npx cap copy
 cordova-res ios --skip-config --copy
 mkdir ios/App/fastlane
 cp mobile/Fastfile ios/App/fastlane
-cp mobile/localizalia/ios/*.plist ios/App
+cp mobile/localizalia/ios/*.plist ios/App/App
 cp mobile/*.rb ios/App
 cp mobile/*.entitlements ios/App/App
 cp mobile/Gemfile* ios/App
