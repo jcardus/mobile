f039b1d8dde17645b6eec7a18c1dafb6d4ec6b10
jcardus <asklocation.net@gmail.com>
2022-05-07 22:55:52 -0400

    build.sh

---
+++ mobile/Localizalia/build.sh
@@ -0,0 +1,7 @@
+export COGNITO_CLIENT_ID=6udglokm8muv3j06tnledmo0ki
+export USER_POOL_ID=us-east-1_olpbc774t
+export AUTH_DOMAIN=auth.localizalia.com
+export APP_NAME=Localizalia
+export PACKAGE_NAME=com.fleetmap.localizalia
+export HOST_NAME=plataforma.localizalia.com
+
