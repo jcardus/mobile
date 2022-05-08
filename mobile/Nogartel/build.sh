6a5758c7a947c597d39383673582c22281f270b8
jcardus <asklocation.net@gmail.com>
2022-05-08 18:31:51 -0400

    adding nogartel

---
+++ mobile/Nogartel/build.sh
@@ -0,0 +1,13 @@
+echo "export nogartel"
+#export USER_POOL_ID=us-east-1_olpbc774t
+export APP_NAME=Nogartel
+export PACKAGE_NAME=com.fleetmap.nogartel
+export HOST_NAME=nogartel.fleetmap.io
+export SOCIAL_SIGN_IN=true
+export COGNITO_CLIENT_ID=3ft0g5mjgt9d35v8d6loe3aemm
+export AUTH_DOMAIN=accounts.fleetmap.io
+
+mobile/build.sh "$1"
+
+
+
