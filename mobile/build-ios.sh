7f5f7cf65c7e9021834955fc7c7fa6771cfb0377
jcardus <asklocation.net@gmail.com>
2022-05-09 01:37:01 -0400

    add push notifications to AppDelegate.swift 2.12.2

--- mobile/build-ios.sh
+++ mobile/build-ios.sh
@@ -8,6 +8,7 @@ cp mobile/"$APP_NAME"/*.plist ios/App/App
 cp mobile/*.rb ios/App
 cp mobile/*.entitlements ios/App/App
 cp mobile/Gemfile* ios/App
+cp mobile/AppDelegate.swift ios/App/App
 
 cd ios/App
 gem install bundler
