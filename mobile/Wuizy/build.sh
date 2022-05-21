36c4ddd88b4c7f01b518290199a1520a5ee3930e
jcardus <asklocation.net@gmail.com>
2022-05-21 12:28:12 -0400

    adding wuizy

---
+++ mobile/Wuizy/build.sh
@@ -0,0 +1,12 @@
+export USER_POOL_ID=us-east-1_SWTiH7d38
+export APP_NAME=Wuizy
+export PACKAGE_NAME=com.fleetmap.wuizy
+export HOST_NAME=wuizy.co.ao
+export SOCIAL_SIGN_IN=true
+export COGNITO_CLIENT_ID=2gcl7rh96fvf73vpo871sb6nb9
+export AUTH_DOMAIN=accounts.fleetmap.io
+
+mobile/build.sh "$1"
+
+
+
