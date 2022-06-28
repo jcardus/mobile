f1294841d9d225b021c63e5cd7dd87cdb24bfb10
jcardus <asklocation.net@gmail.com>
2022-06-28 20:37:00 +0100

    fix alert datetime 2.15.8

--- src/framework7/pages/alerts/AlertDetail.vue
+++ src/framework7/pages/alerts/AlertDetail.vue
@@ -6,7 +6,7 @@
       <f7-list v-if="alert">
         <f7-list-item
           :title="alert.device && alert.device.name"
-          :after="alert.device && alert.device.position && new Date(alert.device.position.fixTime).toLocaleString()"
+          :after="position && new Date(position.fixTime).toLocaleString()"
         />
         <f7-list-item :after="alert.content" />
       </f7-list>
