a971b40cd61e4b00f239db3016f313c8a502e776
jcardus <asklocation.net@gmail.com>
2023-07-23 19:41:53 +0100

    2.32.7

--- mobile/Localizalia/build.sh
+++ mobile/Localizalia/build.sh
@@ -1,5 +1,5 @@
 echo "export localizalia"
-export COGNITO_CLIENT_ID=6udglokm8muv3j06tnledmo0ki
+export COGNITO_CLIENT_ID=14bad1k5truh9sffa2d5m6h0iu
 export USER_POOL_ID=us-east-1_olpbc774t
 export AUTH_DOMAIN=auth.localizalia.com
 export APP_NAME=Localizalia
