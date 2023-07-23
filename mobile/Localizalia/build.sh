acbe95fd23edf5c1a1c8be6770574235c92b28ae
jcardus <asklocation.net@gmail.com>
2023-07-24 00:36:40 +0100

    fix localizalia user pool id

--- mobile/Localizalia/build.sh
+++ mobile/Localizalia/build.sh
@@ -1,6 +1,6 @@
 echo "export localizalia"
 export COGNITO_CLIENT_ID=14bad1k5truh9sffa2d5m6h0iu
-export USER_POOL_ID=us-east-1_olpbc774t
+export USER_POOL_ID=us-east-1_SWTiH7d38
 export AUTH_DOMAIN=auth.localizalia.com
 export APP_NAME=Localizalia
 export PACKAGE_NAME=com.fleetmap.localizalia
