efd3d3c887af7ba069c4fb28edbb81e9905cae1b
jcardus <asklocation.net@gmail.com>
2024-01-29 11:16:39 +0000

    change cognito

--- src/amplify/index.js
+++ src/amplify/index.js
@@ -10,17 +10,16 @@ const host = Capacitor.isNativePlatform() ? 'capacitor' : window.location.hostna
 const partnerData = getPartnerData(host)
 
 export const awsConfig = {
-  aws_project_region: 'us-east-1',
-  aws_cognito_identity_pool_id: 'us-east-1:b886ef89-6a90-4903-96fc-25af82fc629a',
-  aws_cognito_region: 'us-east-1',
-  aws_user_pools_id: Capacitor.isNativePlatform() ? process.env.USER_POOL_ID : partnerData.aws_user_pools_id,
-  aws_user_pools_web_client_id: Capacitor.isNativePlatform() ? process.env.COGNITO_CLIENT_ID : partnerData.cognitoClientId,
+  aws_project_region: 'eu-west-3',
+  aws_cognito_identity_pool_id: process.env.IDENTITY_POOL,
+  aws_cognito_region: 'eu-west-3',
+  aws_user_pools_id: process.env.USER_POOL_ID,
+  aws_user_pools_web_client_id: process.env.COGNITO_CLIENT_ID,
   oauth: {
     redirectSignIn: Capacitor.isNativePlatform() ? 'https://fleetmap.io/' : (location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '') + '/mobile/'),
     redirectSignOut: location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '') + '/',
     domain: Capacitor.isNativePlatform() ? process.env.AUTH_DOMAIN : partnerData.oauth_domain,
     scope: [
-      'phone',
       'email',
       'openid',
       'aws.cognito.signin.user.admin'
