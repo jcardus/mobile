c3ceb603e4a9a249425e7752efce7ff266a932ca
jcardus <asklocation.net@gmail.com>
2024-01-29 23:39:50 +0000

    start ios

--- mobile/Manager/build.sh
+++ mobile/Manager/build.sh
@@ -1,6 +1,3 @@
-export COGNITO_CLIENT_ID=16seahimlsre6ocin0uvivtet2
-export USER_POOL_ID=us-east-1_SWTiH7d38
-export AUTH_DOMAIN=accounts.fleetmap.io
 export APP_NAME=Manager
 export PACKAGE_NAME=com.fleetmap.manager
 export HOST_NAME=fleetmap.io
