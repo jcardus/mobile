2c02100fad8ed66dc7ac8930d309d4df3c50601c
jcardus <asklocation.net@gmail.com>
2024-02-05 02:54:08 +0000

    Update build.sh

--- mobile/Manager/build.sh
+++ mobile/Manager/build.sh
@@ -1,6 +1,6 @@
 export COGNITO_CLIENT_ID=2ml2d0h1qk7q614qc3bclg2alj
 export USER_POOL_ID=eu-west-3_3zjuFkIv8
-export AUTH_DOMAIN=fleetmap.auth.sa-east-1.amazoncognito.com
+export AUTH_DOMAIN=fleetmap.auth.eu-west-3.amazoncognito.com
 export APP_NAME=Manager
 export PACKAGE_NAME=com.fleetmap.manager
 export HOST_NAME=fleetmap.io
