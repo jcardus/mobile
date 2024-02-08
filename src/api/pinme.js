d04a5b3574e526436fad28f0656a92a06db1bbe2
jcardus <asklocation.net@gmail.com>
2024-02-08 18:49:24 +0000

    fix backend

--- src/api/pinme.js
+++ src/api/pinme.js
@@ -2,16 +2,18 @@ import axios from 'axios'
 import { getServerHost } from './index'
 
 const apiName = 'pinmeapi'
-const pinmeApiBaseUrl = 'https://' + getServerHost() + '/' + apiName + '/'
 
 export const pinmeapi = {
   async get(deviceId) {
+    const pinmeApiBaseUrl = 'https://' + getServerHost() + '/' + apiName + '/'
     return await axios.get(pinmeApiBaseUrl + deviceId, { withCredentials: true }).then(d => d.data)
   },
   async getAll() {
+    const pinmeApiBaseUrl = 'https://' + getServerHost() + '/' + apiName + '/'
     return await axios.get(pinmeApiBaseUrl + 'devices/ignitionoff', { withCredentials: true }).then(d => d.data)
   },
   updateDeviceAccumulators(deviceId, accumulators) {
+    const pinmeApiBaseUrl = 'https://' + getServerHost() + '/' + apiName + '/'
     const body = {
       deviceId: deviceId,
       totalDistance: accumulators.totalDistance,
