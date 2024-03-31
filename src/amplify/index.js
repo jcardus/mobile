af8afd4df1456b1fe1d895d74d913a59db73295c
jcardus <asklocation.net@gmail.com>
2024-04-01 00:16:06 +0100

    pool

--- src/amplify/index.js
+++ src/amplify/index.js
@@ -7,7 +7,7 @@ let dataMemory = {}
 
 export const awsConfig = {
   aws_project_region: 'eu-west-3',
-  aws_cognito_identity_pool_id: process.env.IDENTITY_POOL,
+  aws_cognito_identity_pool_id: process.env.IDENTITY_POOL || 'eu-west-3_3zjuFkIv8',
   aws_cognito_region: 'eu-west-3',
   aws_user_pools_id: process.env.USER_POOL_ID,
   aws_user_pools_web_client_id: process.env.COGNITO_CLIENT_ID,
