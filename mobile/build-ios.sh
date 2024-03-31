a255eadee043192f89f5b948510a3772764439bc
jcardus <asklocation.net@gmail.com>
2024-03-31 00:15:41 +0000

    join build with deploy

--- mobile/build-ios.sh
+++ mobile/build-ios.sh
@@ -2,20 +2,19 @@ npx cap add ios
 npx cap sync ios
 npx cap copy
 cordova-res ios --skip-config --copy
+
 mkdir ios/App/fastlane
 cp -v mobile/Fastfile ios/App/fastlane
 cp -v mobile/Matchfile ios/App/fastlane
 cp -v mobile/"$APP_NAME"/*.plist ios/App/App
-cp mobile/*.rb ios/App
-cp mobile/*.entitlements ios/App/App
-cp mobile/Gemfile* ios/App
-cp mobile/AppDelegate.swift ios/App/App
+cp -v mobile/*.rb ios/App
+cp -v mobile/*.entitlements ios/App/App
+cp -v mobile/Gemfile* ios/App
+cp -v mobile/AppDelegate.swift ios/App/App
+mv -v gc_keys.json ios/App
 
 cd ios/App || exit
 gem install bundler
 bundle install
 gem install xcodeproj
-echo "ruby addGooglePlist.rb"
 ruby addGooglePlist.rb
-
-
