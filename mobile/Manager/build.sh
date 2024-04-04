4656c52c92d66987bce6259216f80d9db0a76fbc
jcardus <asklocation.net@gmail.com>
2024-04-04 19:59:51 +0100

    2.40.9

---
+++ mobile/Manager/build.sh
@@ -0,0 +1,10 @@
+export COGNITO_CLIENT_ID=2ml2d0h1qk7q614qc3bclg2alj
+export USER_POOL_ID=eu-west-3_3zjuFkIv8
+export AUTH_DOMAIN=fleetmap.auth.eu-west-3.amazoncognito.com
+export APP_NAME=Manager
+export PACKAGE_NAME=com.fleetmap.manager
+export HOST_NAME=fleetmap.io
+export MAPBOX_TOKEN=pk.eyJ1IjoibWFwYm94LWlvcy1tYW5hZ2VyIiwiYSI6ImNsc3hxcnJhbDA1MngyaW9ldzl3N3E1NnIifQ.DMs8qVULqGGb6JGILvjFcg
+export SERVER_HOST=api2.pinme.io
+
+mobile/build.sh "$1"
