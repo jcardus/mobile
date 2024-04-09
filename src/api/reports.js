9f116728eae497ecb10b91177d54926872ae9ed4
jpenas <jppenas@gmail.com>
2024-04-09 18:09:17 +0100

    fix axios create

--- src/api/reports.js
+++ src/api/reports.js
@@ -1,7 +1,8 @@
 import Reports from 'fleetmap-reports'
 import { getServerHost } from '@/api/index'
+import axios from 'axios'
 
 export const reports = new Reports({
   basePath: 'https://' + getServerHost() + '/api',
   baseOptions: { withCredentials: true }
-})
+}, axios)
