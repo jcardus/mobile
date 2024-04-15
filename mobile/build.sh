100b9708e5412792cbc43e04831b797d9e006058
jcardus <asklocation.net@gmail.com>
2024-04-15 16:37:11 +0100

    fix build

--- mobile/build.sh
+++ mobile/build.sh
@@ -4,21 +4,24 @@ export accessKeyId=AKIA5EPZKGBG77TDPUCE
 
 vue-cli-service build --mode capacitor --dest dist
 
-cp dist/img/logos/$SERVER_HOST.png dist/img/logos/api.pinme.io.png
-cp dist/img/logos/$SERVER_HOST.png dist/img/logos/localhost.png
+cp -v dist/img/logos/$SERVER_HOST.png dist/img/logos/api.pinme.io.png
+cp -v dist/img/logos/$SERVER_HOST.png dist/img/logos/localhost.png
 
 rm -rf ./ios
 rm -rf ./android
 rm -rf resources
 mkdir resources
-echo "cp -r mobile/$APP_NAME/resources/* resources"
-cp -r mobile/"$APP_NAME"/resources/* resources
+cp -vr mobile/"$APP_NAME"/resources/* resources
 
 if [ "$1" = 'android' ]
 then
   echo 'android'
   export ENVIRONMENT=android
   mobile/build-android.sh
+else
+  echo 'ios'
+  export ENVIRONMENT=ios
+  mobile/build-ios.sh
 fi
 
 
