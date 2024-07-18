1ef818f80bbe8a7ddebd47a8555eef9bc5c3e9a8
jcardus <joaquim.cardeira@gmail.com>
2024-07-18 13:15:45 -0300

    showStopDate only on localizalia

--- src/store/modules/settings.js
+++ src/store/modules/settings.js
@@ -1,6 +1,5 @@
 import defaultSettings from '../../settings'
 import Vue from 'vue'
-import { getPartnerData } from 'fleetmap-partners'
 
 const { sidebarLogo } = defaultSettings
 
@@ -11,7 +10,7 @@ const state = {
   maxSpeedType: 'vehicle',
   speedThreshold: '0',
   showLabels: false,
-  showStopDate: getPartnerData().showStopDate,
+  showStopDate: window.location.hostname === 'localizalia.net' || process.env.SERVER_HOST === 'localizalia.net',
   vehicles3d: false,
   followVehicle: false,
   showFullDate: false
